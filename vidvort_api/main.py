8ec6c9a3eefc11f4581a271e09f3cc7cfaa71158
theharshpat <harsh.patel.magic105@gmail.com>
2024-02-15 03:46:32 +0530

    file upload and save locally with random file name

--- vidvort_api/main.py
+++ vidvort_api/main.py
@@ -1,5 +1,8 @@
+from random import choice
+from string import ascii_lowercase
 from fastapi import FastAPI, UploadFile
 import uvicorn
+from pathlib import Path
 
 app = FastAPI()
 
@@ -8,9 +11,19 @@ async def read_root():
     return {"Hello": "World"}
 
 @app.post("/upload-video")
-async def create_upload_video():
-    return {"filename": "video.mp4"}
+async def create_upload_video(file: UploadFile = None):
+    if file is None:
+        return {"filename": "No file"}
 
+    random_str_10 = ''.join(choice(ascii_lowercase) for i in range(5))
+
+    file_name = random_str_10 + Path(file.filename).suffix
+    file_path = Path("files/uploads") / file_name
+    
+    with file_path.open("wb") as buffer:
+        buffer.write(file.file.read())
+
+    return {"filename": file_name}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
